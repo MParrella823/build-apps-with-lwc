e482717e6a3b12d263da62afee87cf4fde1ca5bb
Matt Parrella <mparrella@appirio.com>
2019-09-18 13:42:00 -0600

    commit test

--- force-app/main/default/lwc/helloWebComponent/helloWebComponent.js
+++ force-app/main/default/lwc/helloWebComponent/helloWebComponent.js
@@ -9,7 +9,7 @@ export default class HelloWebComponent extends LightningElement {
 
   currentDate = new Date().toDateString();
   get capitalizedGreeting() {
-    return `Hello ${this.greeting.toUpperCase()}!`;
+    return `Hello ${this.greeting.toUpperCase()}, ya old SOAB!`;
   }
 
 }
